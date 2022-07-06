3b6fb06d09343cafbb8b846c8ca1e1c0e2d8ff07
cljang <clayton.jang@gmail.com>
2022-07-05 23:53:39 -0700

    Branch off feature content code into its own component and styling

---
+++ src/components/FeatureContentItem.jsx
@@ -0,0 +1,82 @@
+import { useState, useRef, useEffect } from 'react';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import codeTheme  from '../global/codeTheme';
+import Paragraph from './Paragraph';
+import ResponsivePicture from './ResponsivePicture';
+
+function FeatureContentItem({featureContentObj}) {
+
+  // Allow large code to be collapsed down
+  const [codeOpen, setCodeOpen] = useState(false)
+
+
+  // Function to handle how different types of feature content is rendered
+  const renderFeatureContent = (featureContentObj) => {
+    // Ensure featureContentObj is defined
+    if (featureContentObj) {
+      // featureContentObj is a WordPress ACF Pro Flexible Content (FC) block, where the type of content is defined using the acf_fc_layout parameter
+      switch (featureContentObj.acf_fc_layout) {
+        // For text_content, input is a text area that autmatically adds <p> tags
+        case "text_content":
+          return (
+            featureContentObj.text && <div className="text-content">
+              <Paragraph text={featureContentObj.text}/>
+            </div>
+          );
+        
+        // For image_content, take a gallery of responsive images from largest to smallest and build a responsive <picture> element
+        case "image_content":
+          return (
+            <ResponsivePicture 
+              className="image-content"
+              imageArray={featureContentObj.image_sources}
+              alt={featureContentObj.alt}
+            />
+          );
+
+        // For code_content, use a Prism.js syntax highlighter to output a code block
+        case "code_content":
+          return (
+            <div 
+              className={`code-content ${codeOpen ? "code-closed" : "code-open"}`} 
+            >
+              { featureContentObj.code && 
+                <SyntaxHighlighter 
+                  language={featureContentObj.language} 
+                  style={codeTheme}
+                  showLineNumbers={true}
+                >
+                  {featureContentObj.code}
+                </SyntaxHighlighter>
+              }
+              <button>{codeOpen ? "Collapse Code" : "Expand Code"}</button>
+            </div>
+          );
+
+        case "video_content":
+        return (
+          <div 
+            className="video-content" 
+          >
+            <video autoPlay muted loop>
+              <source src={featureContentObj.video.url} type={featureContentObj.video.mime_type} />
+            </video>
+          </div>
+        );
+      
+        default:
+          break;
+      }
+    }
+  }
+
+  return (
+    <div
+      className="feature-content-item"
+    >
+      {featureContentObj && renderFeatureContent(featureContentObj)}
+    </div>
+  )
+}
+
+export default FeatureContentItem
